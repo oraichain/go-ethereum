02c413978db9150f5b4f4226be935de480fd2d1d
evgeniy-scherbina <evgeniy.shcherbina.es@gmail.com>
2024-02-06 16:28:31 -0500

    CR's fixes

--- precompile/contract/contract_test.go
+++ precompile/contract/contract_test.go
@@ -1,14 +1,35 @@
-package contract
+package contract_test
 
 import (
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/state"
+	"github.com/ethereum/go-ethereum/precompile/contract"
 	"github.com/stretchr/testify/require"
 )
 
+var (
+	callerAddr   = common.HexToAddress("0xc0ffee254729296a45a3885639AC7E10F9d54979")
+	contractAddr = common.HexToAddress("0x999999cf1046e68e36E1aA2E0E07105eDDD1f08E")
+)
+
+type accessibleState struct {
+	stateDB contract.StateDB
+}
+
+func newAccessibleState(stateDB contract.StateDB) *accessibleState {
+	return &accessibleState{
+		stateDB: stateDB,
+	}
+}
+
+func (s *accessibleState) GetStateDB() contract.StateDB {
+	return s.stateDB
+}
+
 func test(
-	accessibleState AccessibleState,
+	accessibleState contract.AccessibleState,
 	caller common.Address,
 	addr common.Address,
 	input []byte,
@@ -19,20 +40,20 @@ func test(
 }
 
 func TestPrecompileWithDuplicatedFunctionSelector(t *testing.T) {
-	var functions []*StatefulPrecompileFunction
+	var functions []*contract.StatefulPrecompileFunction
 
-	functions = append(functions, NewStatefulPrecompileFunction(
-		MustCalculateFunctionSelector("test(uint256,uint256)"),
+	functions = append(functions, contract.NewStatefulPrecompileFunction(
+		contract.MustCalculateFunctionSelector("test(uint256,uint256)"),
 		test,
 	))
 
-	functions = append(functions, NewStatefulPrecompileFunction(
-		MustCalculateFunctionSelector("test(uint256,uint256)"),
+	functions = append(functions, contract.NewStatefulPrecompileFunction(
+		contract.MustCalculateFunctionSelector("test(uint256,uint256)"),
 		test,
 	))
 
 	// Construct the contract with no fallback function.
-	_, err := NewStatefulPrecompileContract(functions)
+	_, err := contract.NewStatefulPrecompileContract(functions)
 	require.Error(t, err)
 	require.Contains(t, err.Error(), "cannot create stateful precompile with duplicated function selector")
 }
@@ -56,17 +77,70 @@ func TestPrecompileWithInvalidFunctionSelector(t *testing.T) {
 		},
 	} {
 		t.Run(tc.desc, func(t *testing.T) {
-			functions := []*StatefulPrecompileFunction{
-				NewStatefulPrecompileFunction(
+			functions := []*contract.StatefulPrecompileFunction{
+				contract.NewStatefulPrecompileFunction(
 					tc.fnSelector,
 					test,
 				),
 			}
 
 			// Construct the contract with no fallback function.
-			_, err := NewStatefulPrecompileContract(functions)
+			_, err := contract.NewStatefulPrecompileContract(functions)
 			require.Error(t, err)
 			require.Contains(t, err.Error(), "invalid length of function selector")
+		})
+	}
+}
+
+func TestPrecompileInvalidCalls(t *testing.T) {
+	var (
+		stateDB                = state.NewTestStateDB(t)
+		accessibleState        = newAccessibleState(stateDB)
+		suppliedGas     uint64 = 1
+		readOnly        bool
+	)
+
+	functions := []*contract.StatefulPrecompileFunction{
+		contract.NewStatefulPrecompileFunction(
+			contract.MustCalculateFunctionSelector("test()"),
+			test,
+		),
+	}
+	// Construct the contract with no fallback function.
+	precompiledContract, err := contract.NewStatefulPrecompileContract(functions)
+	require.NoError(t, err)
+
+	unexistingFuncInput := contract.MustCalculateFunctionSelector("unexistingFunc()")
+	invalidArgNumInput := contract.MustCalculateFunctionSelector("getSum3(uint256)")
+	shortFuncSelectorInput := []byte("abc")
+
+	for _, tc := range []struct {
+		desc  string
+		input []byte
+		err   string
+	}{
+		{
+			desc:  "test case #1",
+			input: unexistingFuncInput,
+			err:   "invalid function selector",
+		},
+		{
+			desc:  "test case #2",
+			input: invalidArgNumInput,
+			err:   "invalid function selector",
+		},
+		{
+			desc:  "test case #3",
+			input: shortFuncSelectorInput,
+			err:   "missing function selector to precompile",
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			{
+				_, _, err := precompiledContract.Run(accessibleState, callerAddr, contractAddr, tc.input, suppliedGas, readOnly)
+				require.Error(t, err)
+				require.Contains(t, err.Error(), tc.err)
+			}
 		})
 	}
 }
